eac0861883750b75fb511898e383367d3f40687d
aviel <aviel469@gmail.com>
2024-09-11 16:21:26 +0300

    first commit on file practice.js create function removeDuplicates

--- practicejs.js
+++ practicejs.js
@@ -2,3 +2,7 @@ const filterAndSortEvenNumbers = (arr) => {
   const newArr = arr.sort().filter((a) => a % 2 == 0);
   return newArr;
 };
+const removeDuplicates = (arr) => {
+  const newArr = [...new Set(arr)];
+  return newArr;
+};
