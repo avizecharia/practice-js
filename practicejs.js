f9193274490e158ad19633f3efda07f8220a0b34
aviel <aviel469@gmail.com>
2024-09-11 16:32:44 +0300

    first commit on file practice.js create function to save storage at local storage

--- practicejs.js
+++ practicejs.js
@@ -12,3 +12,25 @@ const capitalizeFirstLetter = (str) => {
     x[x.length - 1] != "." ? x[0].toUpperCase() + x.slice(1) : x
   );
 };
+const ListObjecat = [{ theid: 0 }];
+
+const loadTasks = () => {
+  return localStorage.getItem("listObj");
+};
+const saveTasks = () => {
+  window.localStorage.setItem("listObj", JSON.stringify(ListObjecat));
+};
+const addTask = (task) => {
+  const newObj = { id: ListObjecat[0].theid + 1, task };
+  ListObjecat[0].theid += 1;
+  ListObjecat.push(newObj);
+  saveTasks();
+};
+const removeTask = (id) => {
+  for (let i = 0; i < ListObjecat.length; i++) {
+    if (ListObjecat[i].id == id) {
+      ListObjecat.splice(i, 1);
+    }
+  }
+  saveTasks();
+};
