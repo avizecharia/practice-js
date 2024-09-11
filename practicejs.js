eae73751d98bb52ecd445d05b387d71ce8e32c98
aviel <aviel469@gmail.com>
2024-09-11 16:22:36 +0300

    first commit on file practice.js create function capitalizeFirstLetter

--- practicejs.js
+++ practicejs.js
@@ -6,3 +6,9 @@ const removeDuplicates = (arr) => {
   const newArr = [...new Set(arr)];
   return newArr;
 };
+const capitalizeFirstLetter = (str) => {
+  const a = str.split(" ");
+  return a.map((x) =>
+    x[x.length - 1] != "." ? x[0].toUpperCase() + x.slice(1) : x
+  );
+};
